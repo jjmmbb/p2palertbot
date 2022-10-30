f1bea4f77e163a424453820533bfbeb0785e58c8
Nelson Pérez <bilthon@gmail.com>
2022-10-30 12:24:44 -0500

    feature: implements the 'handleAddAlert' function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
-import { Telegraf } from 'telegraf'
+import { Context, Telegraf, NarrowedContext } from 'telegraf'
+import { Update } from 'typegram/update'
 import { Database } from './db'
+import { OrderType } from '@prisma/client'
 
 const BOT_TOKEN = process.env.BOT_TOKEN
+const CURRENCIES = [
+  'USD',
+  'EUR',
+  'ARS',
+  'USDT',
+  'MXN',
+  'CLP',
+  'COL',
+  'BTL',
+  'VES'
+]
+
+const ORDER_TYPES = ['BUY', 'SELL']
+
+const db = new Database()
 
 const main = async () => {
   if (BOT_TOKEN === undefined) {
     console.log('BOT_TOKEN undefined')
     return
   }
-  const db = new Database()
   const bot = new Telegraf(BOT_TOKEN)
   bot.start(async (ctx) => {
     const { update: { message: { from: { id, is_bot } } } } = ctx
@@ -28,8 +44,9 @@ const main = async () => {
   bot.help(
     ctx => ctx.reply('P2P alert bot will allow you to set automated alerts to trading opportunities you might find interesting')
   )
-  bot.command('addalert', ctx => {
-    ctx.reply('add alert selected!')
+  bot.command('addalert', async (ctx) => {
+    const { update: { message: { text, entities }}} = ctx
+    handleAddAlert(ctx, text.split(' '))
   })
   bot.command('listalerts', ctx => ctx.reply('list alerts selected!'))
   bot.command('editalert', ctx => ctx.reply('edit alert selected!'))
@@ -45,6 +62,51 @@ const main = async () => {
   // Enable graceful stop
   process.once('SIGINT', () => bot.stop('SIGINT'))
   process.once('SIGTERM', () => bot.stop('SIGTERM'))
+}
+
+const handleAddAlert = async (
+  ctx: NarrowedContext<Context, Update.MessageUpdate>,
+  args: string[]
+) => {
+  if (args.length !== 4) {
+    await ctx.reply('Invalid arguments, expects: /addalert <currency> <price_delta> <order_type>')
+    await ctx.reply('Ex: /addalert USD 5 BUY')
+    return await ctx.reply('☝️ Sets an alert for buy orders with more than 5% of premium')
+  } 
+  const [cmd, currency, priceDeltaStr, orderTypeStr] = args
+  const priceDelta = parseFloat(priceDeltaStr)
+  if (priceDelta === NaN) {
+    return ctx.reply(
+      'Invalid <premium/discount>, expecting a positive or negative number'
+    )
+  }
+  if (!CURRENCIES.find(c => c.toLowerCase() === currency.toLowerCase())) {
+    return ctx.reply('Invalid <currency> value, expects a currency name. Ex. USD, EUR, etc')
+  }
+  if (!ORDER_TYPES.find(o => o.toLowerCase() === orderTypeStr.toLowerCase())) {
+    return ctx.reply('Invalid <order_type>. Must be either BUY or SELL')
+  }
+  const { update: { message: { from: { id } } } } = ctx
+  const user = await db.findUser(BigInt(id))
+  if (!user) {
+    await ctx.reply('This is weird, it seems we have not been introduced yet!')
+    return await ctx.reply('Please type /start')
+  }
+  const orderType = orderTypeStr.toUpperCase() === OrderType.BUY
+    ? OrderType.BUY : OrderType.SELL
+  try {
+    const existingAlert = await db.findAlert(user.id, currency, orderType)
+    if (existingAlert) {
+      await db.updateAlert(existingAlert.id, priceDelta)
+    } else {
+      await db.addAlert(user.id, currency, priceDelta, orderType)
+    }
+  } catch(err) {
+    console.error('Error while trying to add/update alert: ', err)
+    return await ctx.reply('Error while trying to add/update alert')
+  }
+  const position = orderType === OrderType.BUY ? 'above' : 'below'
+  ctx.reply(`Perfect! I\'ll let you know whenever a ${orderType} order on ${currency} with price ${position} ${priceDelta}% is posted`)
 }
 
 main()
